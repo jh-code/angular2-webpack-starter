8ddaf27a9489f6ef903e82dd3420ce608a1404da
Jared Helgeson <jared@codeninja.guru>
2017-01-18 23:56:32 -0600

    chore(*): remove unneeded imports and dependencies to reduce bundle size
    size does matter

--- app/polyfills.ts
+++ app/polyfills.ts
@@ -1,7 +1,4 @@
 import 'core-js/client/shim';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
-import 'zone.js/dist/long-stack-trace-zone';
 import 'ts-helpers';
-
- Error['stackTraceLimit'] = Infinity;
